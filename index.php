00d4fb86c6e2756442fe0a3ad4ca632191bcd60f
Danitife <boluwatifedaniell29@gmail.com>
2025-09-11 13:12:44 +0100

    add a welcome page to the application

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$greetings = "Welcome to Danitified Banking App";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $greetings ?></h1>
+</body>
+
+</html>
